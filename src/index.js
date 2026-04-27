76b27ae97bd89960b3cb8c139746401e7dc4335e
Kasenhova67 <katya.gusentsova84@gmail.com>
2026-04-27 11:47:29 +0300

    fix: solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,29 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  let res = '';
+  const space = '**********';
+  const len = 10;
+
+  for (let i = 0; i < expr.length; i += len) {
+    const encoded = expr.substr(i, len);
+    if (encoded === space) {
+      res += ' ';
+    }
+    const symbol = encoded.replace(/^0+/, '');
+
+    let morse = '';
+    for (let j = 0; j < symbol.length; j += 2) {
+      const pair = symbol[j] + symbol[j + 1];
+      if (pair === '10') {
+        morse += '.';
+      } else if (pair === '11') {
+        morse += '-';
+      }
+    }
+    res += MORSE_TABLE[morse] || '';
+  }
+  return res;
 };
